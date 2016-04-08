db265ba568c594838a8e58ddcce46d71df4b9a7c
behrensm03 <michaelbehrens@Michaels-MacBook-Pro.local>
2016-04-08 09:39:17 -0400

    First Real Commit
    Congrats to me

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+var container = $('div'),
+    scrollTo = $('#top');
+
+container.scrollTop(
+    scrollTo.offset().top - container.offset().top + container.scrollTop()
+);
+
+container.animate({
+    scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop()
+});
+
